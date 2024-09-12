574d11e7ccff0c42bc3288bb2dd55f94f3ba0bea
JHEIVEN GUERRERO <jheivenguerrero0516@gmail.com>
2024-09-12 12:19:26 +0800

    Add files via upload

---
+++ ACTIVITY 2 BSIT 3F/INTEGRATIVE/Profile.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <title>PROFILE GROUP 6</title>
+        <link rel="stylesheet" href="style.css">
+        <meta http-equiv=”X-UA-Compatible” content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="myscripts.js"></script>
+
+    </head>
+    <body>
+        
+        <header>
+            <h1>Profile design And All Members Part of the Group 6 </h1>
+        </header>
+
+        <br>
+        <br>
+    <nav>
+        <div class="SLR">
+
+            <div class="PRF">
+                <a href="https://www.facebook.com/jheiven.guerrero"><img src="Images/Jheiven.jpg" alt="human pic" class="bgi"></a>
+                <h2>Jheiven Lhiu E Guerrero</h2>
+                <h2>Representative</h2>
+            </div>
+
+            <div class="PRF">
+                <a href="https://www.facebook.com/tiannnn2"><img src="Images/Kristian.jpg" alt="human pic" class="bgi"></a>
+                <h2>Kristian Villota</h2>
+                <h2>Assistant</h2>
+            </div>
+
+            <div class="PRF">
+                <a href="https://www.facebook.com/mitz.domingo.9235"><img src="Images/Clarence.jpg" alt="human pic" class="bgi"></a>
+                <h2>Clarence Angelo Pedloan</h2>
+                <h2>Left hand man</h2>
+            </div>
+
+            <div class="PRF">
+                <a href="https://www.facebook.com/markraven.jugueta"><img src="Images/Raven.jpg" alt="human pic" class="bgi"></a>
+                <h2>Mark Raven Jugueta</h2>
+                <h2>Right hand man</h2>
+            </div>
+
+            <div class="PRF">
+                <a href="https://www.facebook.com/search/top?q=ken%20rainier"><img src="Images/Ken.jpg" alt="human pic" class="bgi"></a>
+                <h2>Ken Rainier</h2>
+                <h2>Left hand man</h2>
+            </div>
+
+        </div>
+
+    </nav>
+
+    <div class="back-frem">
+        <div class="back-chang" onclick="background1()"><img src="Images/wallpaperflare.com_wallpaper (1).jpg" alt="WP"></div>
+        <div class="back-chang" onclick="background2()"><img src="Images/wallpaperflare.com_wallpaper.jpg" alt="WP"></div>
+        <div class="back-chang" onclick="background3()"><img src="Images/wallpaperflare.com_wallpaper (4).jpg" alt="WP"></div>
+        <div class="back-chang" onclick="background4()"><img src="Images/wallpaperflare.com_wallpaper (6).jpg" alt="WP"></div>
+    </div>
+
+    <script>
+         function background1() {
+        document.querySelector('body').style.background = "url('Images/wallpaperflare.com_wallpaper (1).jpg') center center / cover";
+    }
+    function background2() {
+        document.querySelector('body').style.background = "url('Images/wallpaperflare.com_wallpaper.jpg') center center / cover";
+    }
+    function background3() {
+        document.querySelector('body').style.background = "url('Images/wallpaperflare.com_wallpaper (4).jpg') center center / cover";
+    }
+    function background4() {
+        document.querySelector('body').style.background = "url('Images/wallpaperflare.com_wallpaper (6).jpg') center center / cover";
+    }
+    </script>
+    </body>
+</html>
---
+++ ACTIVITY 2 BSIT 3F/INTEGRATIVE/myscript.js
@@ -0,0 +1,17 @@
+document.querySelector('.PRF').addEventListener('click', function() {
+    this.classList.toggle('clicked');
+});
+
+
+function background1() {
+    document.querySelector('body').style.background = "url('Images/wallpaperflare.com_wallpaper (1).jpg') center center / cover";
+}
+function background2() {
+    document.querySelector('body').style.background = "url('Images/wallpaperflare.com_wallpaper.jpg') center center / cover";
+}
+function background3() {
+    document.querySelector('body').style.background = "url('Images/wallpaperflare.com_wallpaper (4).jpg') center center / cover";
+}
+function background4() {
+    document.querySelector('body').style.background = "url('Images/wallpaperflare.com_wallpaper (6).jpg') center center / cover";
+}
